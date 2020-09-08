c754dec123c02f47d8dd2ba68cebd0b6b3e5aefb
Chukwuemeka Jennifer <jennichuks9@gmail.com]>
2020-09-08 18:53:57 +0100

    Initial commit

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var countDown = 20
+    var timer = Timer()
+    @IBOutlet weak var countDownLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countDownLabel.text = "\(countDown)"
+    
+        
+
+    
     }
 
-
+    @IBAction func startCountDown(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDownMethod), userInfo: nil, repeats: true)
+    }
+    @objc func countDownMethod() {
+        if countDown == 0 {
+            timer.invalidate()
+            return
+            
+        }
+        countDown -= 1
+        countDownLabel.text = "\(countDown)"
+        
+    }
+    
 }
 
